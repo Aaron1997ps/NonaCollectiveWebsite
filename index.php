5e02c9395e6b799f0488e712f796c79b5f292a34
Aaron Mahlke <aaron.mahlke@gmail.com>
2017-11-09 23:45:27 +0100

    index structure a bit

--- index.php
+++ index.php
@@ -7,7 +7,17 @@
     </head>
 
     <body>
-        <h1>(╯°□°）╯︵ ┻━┻</h1>
+        <div class="m-wrap">
+            <header class="m-header">
+                <nav class="m-navigation"></nav>
+            </header>
+            <main class="m-view m-view-main">
+
+                <div class="m-landing">
+                    
+                </div>
+            </main>
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
         <script src="assets/js/main.js"></script>
