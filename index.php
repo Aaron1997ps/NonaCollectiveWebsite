568e546e50a57bd4d9283c287fb751319ea71d97
Felix Hromadko <felix@wildworld.at>
2017-11-20 22:37:17 +0100

    scroll

--- index.php
+++ index.php
@@ -95,5 +95,6 @@
         <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
         <script src="assets/js/main.js"></script>
         <script src="assets/js/landingParallax.js"></script>
+        <script src="assets/js/smoothScrolling.js"></script>
     </body>
 </html>
