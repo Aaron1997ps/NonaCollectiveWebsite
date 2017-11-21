0afb64d0d625bfe05714816935354f525187240c
Felix Hromadko <felix@wildworld.at>
2017-11-21 09:46:33 +0100

    Disable smooth scrolling on mobile/mac

--- assets/js/smoothScrolling.js
+++ assets/js/smoothScrolling.js
@@ -1,10 +1,37 @@
 var speed = 150;
 var distance = 50;
 
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+    }
+
+    return os;
+}
 
 $(document).ready(function () {
     if (window.navigator.msPointerEnabled)
         return false;
+
+    if (getOS() !== 'Windows') {
+        return false;
+    }
+
 
     var where = $(document).scrollTop();
     var time;
