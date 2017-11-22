df07c0f27071923999ec046bb370965f14c193e0
Aaron Mahlke <aaron.mahlke@gmail.com>
2017-11-22 22:53:17 +0100

    cleanup of slider branch

--- index.php
+++ index.php
@@ -13,26 +13,7 @@
             <main class="m-view m-view-main">
 
                 <?php include("modules/landing.php") ?>
-
-
-                <div class="m-module m-element-slider">
-                    <div class="m-module-content">
-                        <img class="m-arrow m-left" src="assets/img/icons/amaranth-arrow-left.svg">
-                        <img class="m-arrow m-right" src="assets/img/icons/amaranth-arrow-right.svg">
-                        <div class="m-elements-container">
-                            <img class="m-element m-01" src="assets/img/player/earth.png">
-                            <img class="m-element m-02" src="assets/img/player/fire.png">
-                            <img class="m-element m-03" src="assets/img/player/ice.png">
-                            <img class="m-element m-04" src="assets/img/player/neutral.png">
-                            <img class="m-element m-05" src="assets/img/player/metal.png">
-                            <img class="m-element m-06" src="assets/img/player/water.png">
-                            <img class="m-element m-07" src="assets/img/player/shadow.png">
-                            <img class="m-element m-08" src="assets/img/player/nature.png">
-                            <img class="m-element m-09" src="assets/img/player/light.png">
-                        </div>
-                    </div>
-                </div>
-
+                <?php include("modules/elementSlider.php") ?>
                 <?php include("modules/updates.php") ?>
                 <?php include("modules/discordInvitation.php") ?>
                 <?php include("modules/footer.php") ?>
