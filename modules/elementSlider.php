daa203c386ac33f137ba78b50b033a8b4a1ce6f9
Aaron Mahlke <aaron.mahlke@gmail.com>
2017-11-24 00:01:45 +0100

    slider background fix

--- modules/elementSlider.php
+++ modules/elementSlider.php
@@ -1,8 +1,8 @@
 <div class="m-module m-element-slider">
     <div class="m-module-content">
+        <img class="m-arrow m-left" src="assets/img/icons/amaranth-arrow-left.svg">
+        <img class="m-arrow m-right" src="assets/img/icons/amaranth-arrow-right.svg">
         <div class="m-elements-container">
-            <img class="m-arrow m-left" src="assets/img/icons/amaranth-arrow-left.svg">
-            <img class="m-arrow m-right" src="assets/img/icons/amaranth-arrow-right.svg">
             <img class="m-element m-01" src="assets/img/player/earth.png">
             <img class="m-element m-02" src="assets/img/player/fire.png">
             <img class="m-element m-03" src="assets/img/player/ice.png">
