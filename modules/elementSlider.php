4259c6b45030a1fca1f74b6efb7812074529cc23
Drew <drew020508@gmail.com>
2017-11-23 20:31:00 -0500

    Added the whoel slider minus the gradient

--- modules/elementSlider.php
+++ modules/elementSlider.php
@@ -2,6 +2,11 @@
     <div class="m-module-content">
         <img class="m-arrow m-left" src="assets/img/icons/amaranth-arrow-left.svg">
         <img class="m-arrow m-right" src="assets/img/icons/amaranth-arrow-right.svg">
+        <div class='m-elements-text-container'>
+          <h3 class='m-text-title'>Metal</h3>
+          <img class='m-down-arrow' src='assets/img/icons/slider-arrow-down.svg'>
+          <p class='m-text-desc'>Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares. (Aaron does, duh)</p>
+        </div>
         <div class="m-elements-container">
             <img class="m-element m-01" src="assets/img/player/earth.png">
             <img class="m-element m-02" src="assets/img/player/fire.png">
