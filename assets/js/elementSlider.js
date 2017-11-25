244458dbfe6069c69cc397dd15a702a50591383f
Felix Hromadko <felix@wildworld.at>
2017-11-25 13:52:51 +0100

    Slider changes

--- assets/js/elementSlider.js
+++ assets/js/elementSlider.js
@@ -10,25 +10,58 @@ $(document).ready(function () {
     slider.find('.m-right').click(function () {
         changeElements(-1);
     });
+
+    updateClick();
 });
 
+function updateClick() {
+    //unbind all clicking functions
+    slider.find('.m-element').off();
+
+    for (var i = 1; i <= 9; i++) {
+        var element = slider.find('.m-0' + i);
+
+        element.attr('index', i);
+        console.log(element.attr('index'));
+
+
+        element.click(function () {
+            console.log(5 - $(this).attr('index'));
+            changeElements(5 - $(this).attr('index'));
+        });
+    }
+}
+
 function changeElements(change) {
+    if (change === 0) return;
     var tmp = [];
 
     for (var i = 1; i <= 9; i++) {
         tmp[i] = slider.find('.m-0' + i);
-        console.log(tmp[i]);
     }
 
 
     for (var i = 1; i <= 9; i++) {
+        console.log(i);
         var element = tmp[i];
-        console.log(element);
+
         var ne = (i + change);
-        if (ne > 9) ne = 1;
-        if (ne < 1) ne = 9;
+        if (ne > 9) ne = ne - 9;
+        if (ne < 1) ne = ne + 9;
 
         element.addClass('m-0' + ne);
         element.removeClass('m-0' + i);
+
     }
+
+    //get current element
+    var current = slider.find('.m-05');
+
+    //set name
+    slider.find('.m-text-title').text(current.attr('name'));
+
+    //set desc
+    slider.find('.m-text-desc').text(current.attr('description'));
+
+    updateClick();
 }
--- modules/elementSlider.php
+++ modules/elementSlider.php
@@ -8,15 +8,15 @@
           <p class='m-text-desc'>Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares. (Aaron does, duh)</p>
         </div>
         <div class="m-elements-container">
-            <img class="m-element m-01" src="assets/img/player/earth.png">
-            <img class="m-element m-02" src="assets/img/player/fire.png">
-            <img class="m-element m-03" src="assets/img/player/ice.png">
-            <img class="m-element m-04" src="assets/img/player/neutral.png">
-            <img class="m-element m-05" src="assets/img/player/metal.png">
-            <img class="m-element m-06" src="assets/img/player/water.png">
-            <img class="m-element m-07" src="assets/img/player/shadow.png">
-            <img class="m-element m-08" src="assets/img/player/nature.png">
-            <img class="m-element m-09" src="assets/img/player/light.png">
+            <img class="m-element m-01" src="assets/img/player/earth.png" name="Earth" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares. (Aaron does, duh)">
+            <img class="m-element m-02" src="assets/img/player/fire.png" name="fire" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
+            <img class="m-element m-03" src="assets/img/player/ice.png" name="ice" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
+            <img class="m-element m-04" src="assets/img/player/neutral.png" name="neutral" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
+            <img class="m-element m-05" src="assets/img/player/metal.png" name="metal" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
+            <img class="m-element m-06" src="assets/img/player/water.png" name="water" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
+            <img class="m-element m-07" src="assets/img/player/shadow.png" name="shadow" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
+            <img class="m-element m-08" src="assets/img/player/nature.png" name="nature" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
+            <img class="m-element m-09" src="assets/img/player/light.png" name="light" description="Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone..">
         </div>
     </div>
 </div>
