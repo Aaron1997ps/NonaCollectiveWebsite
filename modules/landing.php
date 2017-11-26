ce7b805ed12dcfdbb6be13283054942a8b68d530
Felix Hromadko <felix@wildworld.at>
2017-11-26 15:13:30 +0100

    max scroll height fix

--- modules/landing.php
+++ modules/landing.php
@@ -8,10 +8,6 @@
         <span class="m-text">Discover Amaranth</span>
     </a>
 
-    <a class='m-landing-button' href='#updates' style="left: 500px">
-        <span class="m-icon"><img src="../assets/img/icons/arrow-down-simple.svg" alt=""></span>
-        <span class="m-text">Discover updates</span>
-    </a>
     <div class="m-parallax-container">
         <div class="m-canvas">
             <div class="m-sky-gradient"></div>
