261f13376d4d4054553517d48b7a59a644dc95b2
Aaron Mahlke <aaron.mahlke@gmail.com>
2017-11-26 21:44:17 +0100

    viewport width / slider hover

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,400i,500,600,700,700i,800,800i" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/main.css">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
 
     <body>
