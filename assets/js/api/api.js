1a03b50f7e8971f0075d33315fca39e12c540bc6
Felix Hromadko <felix@wildworld.at>
2017-11-27 14:50:49 +0100

    Api + login i guess :thinking:

---
+++ assets/js/api/api.js
@@ -0,0 +1,70 @@
+var api = {
+    send: function (url, param, call) {
+        console.log(url);
+        $.ajax({
+            type: "POST",
+            url: '/api/v1' + url,
+            data: param,
+            success: function (data) {
+                console.log(data);
+
+                if(typeof call === 'function')
+                    call(data);
+            },
+            dataType: 'json'
+        });
+    },
+
+    auth: {
+        endpoint: "/auth/",
+        send: function (url, param, call) {
+            api.send(this.endpoint + url, param, call)
+        },
+        login: function (user, password, call) {
+            //password = sha256(password);
+            this.send("login", {username: user, password: password}, call)
+        },
+        logout: function (call) {
+            this.send("logout", {}, call)
+        }
+    },
+
+    elements: {
+        endpoint: "/elements/",
+        send: function (url, param, call) {
+            api.send(this.endpoint + url, param, call)
+        },
+        getDescription: function (name, call) {
+            this.send("getDescription", {name: name}, call)
+        },
+        setDescription: function (name, description, call) {
+            this.send("setDescription", {name: name, description: description}, call)
+        }
+    }
+
+};
+
+/*
+api.auth.login('reolat', 'hashed', function (data) {
+    if (data.success) {
+
+        api.elements.setDescription('water', 'wasser ist toll! ' + Math.random(), function (data) {
+            if (data.success) {
+                api.elements.getDescription('water', function (data) {
+                    if (data.success) {
+                        console.log("----->" + data.response);
+                        api.auth.logout(function () {
+                            if (data.success) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+    }
+});*/
+
+
+
--- login.php
+++ login.php
@@ -16,14 +16,14 @@
             <div class="m-head">
                 <div class="m-content">
                     <img src="assets/img/login/logo.png" class="m-logo">
-                    <div class="m-inputs">
-                        <input type="text" placeholder="USERNAME" class="m-input">
-                        <input type="password" placeholder="PASSWORD" class="m-input">
-                    </div>
-                    <div class="m-buttons">
-                        <div class="m-forgot">forgot your password?</div>
-                        <div class="m-login">LOGIN<img src="assets/img/login/arrow.png"></div>
-                    </div>
+                        <div class="m-inputs">
+                            <input type="text" placeholder="USERNAME" class="m-input m-username">
+                            <input type="password" placeholder="PASSWORD" class="m-input m-password">
+                        </div>
+                        <div class="m-buttons">
+                            <div class="m-forgot">forgot your password?</div>
+                            <div class="m-login">LOGIN<img src="assets/img/login/arrow.png"></div>
+                        </div>
                 </div>
             </div>
             <div class="m-footer">
@@ -31,5 +31,12 @@
             </div>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <script src="assets/js/sha256.js"></script>
+    <script src="assets/js/loader.js"></script>
+    <script src="assets/js/ui/label.js"></script>
+    <script src="assets/js/login.js"></script>
+    <script src="assets/js/api/api.js"></script>
 </body>
 </html>
