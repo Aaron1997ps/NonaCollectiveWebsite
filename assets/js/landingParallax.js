81112f2d356a16a4c9eba22846881857c189a2ee
Felix Hromadko <felix@wildworld.at>
2017-11-27 19:18:22 +0100

    Make that it works in 90% of the cases.  3:30.

--- assets/js/landingParallax.js
+++ assets/js/landingParallax.js
@@ -1,30 +1,21 @@
 $(document).ready(function () {
-    for (var i = 1; i <= 16; i++) {
-        var index = i;
-
-        while (index > 8) {
-            index -= 8;
-        }
-
+    for (var i = 1; i <= 8; i++) {
         var speed = Math.random() * 5 + 1;
         var height = Math.random() * 40;
-        var width = Math.random() * 100;
-        var delay = Math.random() * 25;
+        var duration = getRandomArbitrary(30, 60);
+        var delay = getRandomArbitrary(0, duration);
+
+        spawnCloud(speed, height, -delay, duration)
+    }
+
+    setInterval(function () {
+        var speed = Math.random() * 5 + 1;
+        var height = Math.random() * 40;
+        var delay = Math.random() * 5;
         var duration = getRandomArbitrary(30, 60);
 
-        $('.m-clouds').append('<img class="m-cloud-' + i + '" src="assets/img/nature_scene/clouds/Sunny' + index + '.png">');
-        var cloud = $('.m-cloud-' + i);
-
-        cloud.css({
-            'width': speed + 'em',
-            'height': 'auto',
-            'top': height + '%',
-            'left':  '-20%',
-            'position': 'absolute',
-            'animation-delay' : delay + 's',
-            'animation-duration' : duration + 's'
-        });
-    }
+        spawnCloud(speed, height, 0, duration)
+    }, 2000);
 
     $(window).scroll(function () {
 
@@ -38,6 +29,34 @@ $(document).ready(function () {
         back.css({'top': currentScroll / 2 + 'px'});
     });
 });
+
+var cloudI = 1;
+var i = 1;
+function spawnCloud(speed, height, delay, duration) {
+    while (cloudI > 8) {
+        cloudI -= 8;
+    }
+
+    $('.m-clouds').append('<img class="m-cloud-' + i + '" src="assets/img/nature_scene/clouds/Sunny' + cloudI + '.png">');
+    var cloud = $('.m-cloud-' + i);
+
+    cloud.css({
+        'width': speed + 'em',
+        'height': 'auto',
+        'top': height + '%',
+        'left':  '-20%',
+        'position': 'absolute',
+        'animation-duration' : duration + 's',
+        'animation-delay' : delay + 's'
+    });
+
+    setTimeout(function () {
+        cloud.remove();
+    }, duration *1000 + 10);
+
+    cloudI++;
+    i++;
+}
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
