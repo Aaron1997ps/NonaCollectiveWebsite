a7671cf1152cc76021474a48d034e8a9395211b1
Felix Hromadko <felix@wildworld.at>
2017-12-01 20:24:36 +0100

    improoved loading time

--- modules/elementSlider.php
+++ modules/elementSlider.php
@@ -1,8 +1,6 @@
 <?php
 
-function prnt($name) {
-    echo(MDatabaseElement::getDescription($name));
-}
+$elements = MDatabaseElement::getAllDescriptions();
 ?>
 
 <div class="m-module m-element-slider m-inset" id="slider">
@@ -15,15 +13,16 @@ function prnt($name) {
             <p class='m-text-desc'>Look at this cool stone.. Its not a stone, its metal but who cares. Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares.Look at this cool stone.. Its not a stone, its metal but who cares. (Aaron does, duh)</p>
         </div>
         <div class="m-elements-container">
-            <img class="m-element m-01" src="assets/img/player/earth.png" name="Earth"      description="<?php prnt("Earth")?>">
-            <img class="m-element m-02" src="assets/img/player/fire.png" name="Fire"        description="<?php prnt("Fire")?>">
-            <img class="m-element m-03" src="assets/img/player/ice.png" name="Ice"          description="<?php prnt("Ice")?>">
-            <img class="m-element m-04" src="assets/img/player/neutral.png" name="Neutral"  description="<?php prnt("Neutral")?>">
-            <img class="m-element m-05" src="assets/img/player/metal.png" name="Metal"      description="<?php prnt("Metal")?>">
-            <img class="m-element m-06" src="assets/img/player/water.png" name="Water"      description="<?php prnt("Water")?>">
-            <img class="m-element m-07" src="assets/img/player/shadow.png" name="Shadow"    description="<?php prnt("Shadow")?>">
-            <img class="m-element m-08" src="assets/img/player/nature.png" name="Nature"    description="<?php prnt("Nature")?>">
-            <img class="m-element m-09" src="assets/img/player/light.png" name="Light"      description="<?php prnt("Light")?>">
+            <?php
+            $i = 1;
+            foreach (array_keys($elements) as $element) {
+                ?>
+                <img class="m-element m-0<?php echo($i);?>" src="assets/img/player/<?php echo($element) ?>.png" name="<?php echo(ucwords($element)) ?>" description="<?php echo($elements[$element]) ?>">
+                <?php
+                $i++;
+            }
+            ?>
+
         </div>
     </div>
 </div>
