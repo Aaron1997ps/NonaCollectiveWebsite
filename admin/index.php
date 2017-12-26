708c745cc0585cbefc1378f5d1f2a68d51ce42b8
Felix Hromadko <felix@wildworld.at>
2017-12-26 19:41:49 +0100

    Fixed login sass

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,7 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,400i,500,600,700,700i,800,800i" rel="stylesheet">
 
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/admin/admin.css">
     <title></title>
 </head>
 
